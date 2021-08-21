2b2f3b572cc5af2b1a9abb67dbadb1bd6ac76ecf
jefferson1208 <jefferson.souza1208@hotmail.com>
2021-08-20 23:42:41 -0300

    feat: adicao validacao tipos numericos

--- src/EasyValidator.Validator/Validations/Number.cs
+++ src/EasyValidator.Validator/Validations/Number.cs
@@ -4,39 +4,44 @@ namespace EasyValidator.Validator.Validations
 {
     public partial class EasyValidatorContract<T>
     {
-        public EasyValidatorContract<T> IsGreater(dynamic val, dynamic comparer, string message)
+        #region "decimal"
+
+        /* decimal */
+
+        
+        public EasyValidatorContract<T> IsGreater(decimal val, decimal comparer, string message)
         {
-            if ((double)val <= comparer)
+            if (val <= comparer)
                 AddError(message);
 
             return this;
         }
 
-        public EasyValidatorContract<T> IsGreaterOrEquals(dynamic val, dynamic comparer, string message)
+        public EasyValidatorContract<T> IsGreaterOrEquals(decimal val, decimal comparer, string message)
         {
-            if ((double)val < comparer)
+            if (val < comparer)
                 AddError(message);
 
             return this;
         }
 
-        public EasyValidatorContract<T> IsLower(dynamic val, dynamic comparer, string message)
+        public EasyValidatorContract<T> IsLower(decimal val, decimal comparer, string message)
         {
-            if ((double)val >= comparer)
+            if (val >= comparer)
                 AddError(message);
 
             return this;
         }
 
-        public EasyValidatorContract<T> IsLowerOrEquals(dynamic val, dynamic comparer, string message)
+        public EasyValidatorContract<T> IsLowerOrEquals(decimal val, decimal comparer, string message)
         {
-            if ((double)val > comparer)
+            if (val > comparer)
                 AddError(message);
 
             return this;
         }
 
-        public EasyValidatorContract<T> AreEquals(dynamic val, dynamic comparer, string message)
+        public EasyValidatorContract<T> AreEquals(decimal val, decimal comparer, string message)
         {
             if (val != comparer)
                 AddError(message);
@@ -44,7 +49,7 @@ namespace EasyValidator.Validator.Validations
             return this;
         }
 
-        public EasyValidatorContract<T> AreNotEquals(dynamic val, dynamic comparer, string message)
+        public EasyValidatorContract<T> AreNotEquals(decimal val, decimal comparer, string message)
         {
             if (val == comparer)
                 AddError(message);
@@ -52,7 +57,175 @@ namespace EasyValidator.Validator.Validations
             return this;
         }
 
-        public EasyValidatorContract<T> IsBetween(dynamic val, dynamic from, dynamic to, string message)
+        public EasyValidatorContract<T> IsBetween(decimal val, decimal from, decimal to, string message)
+        {
+            if (!(val > from && val < to))
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> ContainsInList(decimal val, IList<decimal> comparer, string message)
+        {
+            var index = comparer.IndexOf(val);
+
+            if (index < 0)
+                AddError(message);
+
+            return this;
+        }
+
+
+        public EasyValidatorContract<T> NotContainsInList(decimal val, IList<decimal> comparer, string message)
+        {
+            var index = comparer.IndexOf(val);
+
+            if (index >= 0)
+                AddError(message);
+
+            return this;
+        }
+
+        #endregion
+
+        #region "double"
+
+        /* decimal */
+
+
+        public EasyValidatorContract<T> IsGreater(double val, double comparer, string message)
+        {
+            if (val <= comparer)
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> IsGreaterOrEquals(double val, double comparer, string message)
+        {
+            if (val < comparer)
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> IsLower(double val, double comparer, string message)
+        {
+            if (val >= comparer)
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> IsLowerOrEquals(double val, double comparer, string message)
+        {
+            if (val > comparer)
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> AreEquals(double val, double comparer, string message)
+        {
+            if (val != comparer)
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> AreNotEquals(double val, double comparer, string message)
+        {
+            if (val == comparer)
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> IsBetween(double val, double from, double to, double message)
+        {
+            if (!(val > from && val < to))
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> ContainsInList(double val, IList<double> comparer, string message)
+        {
+            var index = comparer.IndexOf(val);
+
+            if (index < 0)
+                AddError(message);
+
+            return this;
+        }
+
+
+        public EasyValidatorContract<T> NotContainsInList(double val, IList<double> comparer, string message)
+        {
+            var index = comparer.IndexOf(val);
+
+            if (index >= 0)
+                AddError(message);
+
+            return this;
+        }
+
+        #endregion
+
+        #region "int"
+
+        /* decimal */
+
+
+        public EasyValidatorContract<T> IsGreater(int val, int comparer, string message)
+        {
+            if (val <= comparer)
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> IsGreaterOrEquals(int val, int comparer, string message)
+        {
+            if (val < comparer)
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> IsLower(int val, int comparer, string message)
+        {
+            if (val >= comparer)
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> IsLowerOrEquals(int val, int comparer, string message)
+        {
+            if (val > comparer)
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> AreEquals(int val, int comparer, string message)
+        {
+            if (val != comparer)
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> AreNotEquals(int val, int comparer, string message)
+        {
+            if (val == comparer)
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> IsBetween(int val, int from, int to, string message)
         {
             if (!(val > from && val < to))
                 AddError(message);
@@ -81,7 +254,70 @@ namespace EasyValidator.Validator.Validations
             return this;
         }
 
-        public EasyValidatorContract<T> ContainsInList(decimal val, IList<decimal> comparer, string message)
+        #endregion
+
+        #region "float"
+
+        /* decimal */
+
+
+        public EasyValidatorContract<T> IsGreater(float val, float comparer, string message)
+        {
+            if (val <= comparer)
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> IsGreaterOrEquals(float val, float comparer, string message)
+        {
+            if (val < comparer)
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> IsLower(float val, float comparer, string message)
+        {
+            if (val >= comparer)
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> IsLowerOrEquals(float val, float comparer, string message)
+        {
+            if (val > comparer)
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> AreEquals(float val, float comparer, string message)
+        {
+            if (val != comparer)
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> AreNotEquals(float val, float comparer, string message)
+        {
+            if (val == comparer)
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> IsBetween(float val, float from, float to, string message)
+        {
+            if (!(val > from && val < to))
+                AddError(message);
+
+            return this;
+        }
+
+        public EasyValidatorContract<T> ContainsInList(float val, IList<float> comparer, string message)
         {
             var index = comparer.IndexOf(val);
 
@@ -92,7 +328,7 @@ namespace EasyValidator.Validator.Validations
         }
 
 
-        public EasyValidatorContract<T> NotContainsInList(decimal val, IList<decimal> comparer, string message)
+        public EasyValidatorContract<T> NotContainsInList(float val, IList<float> comparer, string message)
         {
             var index = comparer.IndexOf(val);
 
@@ -102,35 +338,8 @@ namespace EasyValidator.Validator.Validations
             return this;
         }
 
-        public EasyValidatorContract<T> ContainsInList(double val, IList<double> comparer, string message)
-        {
-            var index = comparer.IndexOf(val);
-
-            if (index < 0)
-                AddError(message);
-
-            return this;
-        }
-
-
-        public EasyValidatorContract<T> NotContainsInList(double val, IList<double> comparer, string message)
-        {
-            var index = comparer.IndexOf(val);
-
-            if (index >= 0)
-                AddError(message);
-
-            return this;
-        }
-
-        public EasyValidatorContract<T> OneOrAnother(dynamic priorityValue, dynamic secundaryValue, string message)
-        {
-            var value = priorityValue + secundaryValue;
-
-            if (value <= 0)
-                AddError(message);
-
-            return this;
-        }
+        #endregion
     }
+
+
 }
